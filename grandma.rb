9820daa63d621bac39f86c76ba740004275b8acc
Luis Herrera <herrera.lrh@gmail.com>
2019-10-25 23:21:37 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,15 @@
+require 'pry'
+def uppercase?(phrase)
+  phrase == phrase.upcase
+end
+
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  end
-  if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif uppercase?(phrase)
+    "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
